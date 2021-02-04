e0a7ac6aa75de4066318d4e0d49bb492fab56719
Vo Nhat Truong <truong.vo@saigontechnology.com>
2021-02-05 01:37:07 +0700

    implement create new board feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Redirect, Switch } from 'react-router-dom';
 import { routeConfig, RouteWithSubRoutes } from 'router/config';
 import { PageEnum } from 'router/page.enum';
+import { HelperServices } from 'services/helper';
+import { shallowEqual, useSelector } from 'react-redux';
+import { selectUserInfo } from 'pages/Authenticate/Login/store/selector';
+import axiosInstance from 'api/axios-config';
+import { DEVICE_ENDPOINT } from 'api/endpoint';
+import { LocalStoreService } from 'services/localStore';
+import { LOCAL_KEY } from 'constant/localstore.key';
+
+enum Permission {
+  Grant = 'granted',
+  Default = 'default',
+}
 
 function App() {
+  const userInfo = useSelector(selectUserInfo, shallowEqual);
+  const helperService = new HelperServices();
+  const localStorage = new LocalStoreService();
+
+  const registerUserDevice = () => {
+    if (!helperService.isNotEmptyObject(userInfo)) return;
+
+    const localDeviceToken = localStorage.getString(
+      LOCAL_KEY.DEVICE_TOKEN_ONE_SIGNAL
+    );
+
+    if (localDeviceToken) return;
+
+    OneSignal.getUserId(function(deviceToken: string) {
+      if (deviceToken) {
+        axiosInstance
+          .post(DEVICE_ENDPOINT.REGISTER, {
+            deviceToken,
+          })
+          .then(() =>
+            localStorage.set<string>(
+              LOCAL_KEY.DEVICE_TOKEN_ONE_SIGNAL,
+              deviceToken
+            )
+          );
+      }
+    });
+  };
+
+  useEffect(() => {
+    helperService.loadJs(
+      'https://cdn.onesignal.com/sdks/OneSignalSDK.js',
+      () => {
+        if (window !== undefined && OneSignal) {
+          OneSignal.push(() => {
+            OneSignal.init({
+              appId: process.env.REACT_APP_ONESIGNAL_APP_ID,
+            });
+
+            OneSignal.getNotificationPermission((permission: Permission) => {
+              console.log('permission', permission);
+            });
+
+            registerUserDevice();
+          });
+        }
+      }
+    );
+  }, []);
+
   return (
     <>
       <Switch>
